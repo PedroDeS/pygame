cdff33571474243450b38246545609e765079287
Pedro DeSouza <pedrojdesouza@gmail.com>
2025-01-14 00:16:48 -0500

    E: Updated Player groups in main, cleaned the code

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
-#Initialize pygame, set screen size, set clock and delta variables
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-#Create infinite loop for game to stay open, end code loop when window is closed 
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
 
-    #Player Movement
-        #Rotate Player
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
 
-#Screen fill color and screen refresh
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-# limit the framerate to 60 FPS
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-
 
 
 if __name__ == "__main__":
