ddca4eee174040ea74ff3a1e99752b36e922b1bc
Pedro DeSouza <pedrojdesouza@gmail.com>
2025-01-13 23:24:28 -0500

    B: player created and updated related files

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pygame
 from constants import *
+from player import *
 
+#Initialize pygame, set screen size, set clock and delta variables
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.display.set_mode()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+#Create infinite loop for game to stay open, end code loop when window is closed 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+
+#Screen fill color and screen refresh
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+# limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
+
 
 if __name__ == "__main__":
     main()
